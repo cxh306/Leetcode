c40f7a391ae505788382a587d4f7b29c06509c39
cxh <763591761@qq.com>
2021-08-18 21:34:20 +0800

    8-18

---
+++ code/src/Stack/Solution456.java
@@ -0,0 +1,29 @@
+package Stack;
+
+import org.junit.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution456 {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> d = new LinkedList<>();
+        int k = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < k) return true;
+            while (!d.isEmpty() && d.peek() < nums[i]) {
+                // 事实上，k 的变化也具有单调性，直接使用 k = pollLast() 也是可以的
+                k = Math.max(k, d.pop());
+            }
+            d.push(nums[i]);
+        }
+        return false;
+    }
+
+    @Test
+    public void test(){
+        int[] num = {1,3,2,4,5,6,7,8,9,10};
+        System.out.println(find132pattern(num));
+    }
+}
